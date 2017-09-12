66b467129aed45954033dcf3ac1239d76e8de698
jprestor <vsezabili@gmail.com>
2017-09-12 09:16:13 +0300

    Настроил watcher (less, mincss)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
-var gulp = require('gulp')
-var less = require('gulp-less');
 
-gulp.task("less", function() {
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
+
+//сборка less
+gulp.task('less', function() {
 	gulp.src("less/style.less")
 		.pipe(less())
 		.pipe(gulp.dest("css"));
 });
+
+//Минификация css
+gulp.task('mincss', function(){
+  gulp.src('css/style.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('css'));
+});
+
+//Отслеживание
+gulp.task('watcher',function(){
+	  gulp.watch('less/styl.less', ['less']);
+    gulp.watch('css/style.css', ['mincss']);
+})
